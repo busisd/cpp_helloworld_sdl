22000a8848d2a7700caca14d6199a59156334e46
Daniel Busis <busis.daniel@gmail.com>
2022-09-04 19:31:04 -0400

    Game now runs on a clock

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -19,13 +19,13 @@ void print_set(std::unordered_set<T> const &s)
 /**
  * https://lazyfoo.net/tutorials/SDL/07_texture_loading_and_rendering/index.php
  */
-SDL_Texture* loadTexture( std::string path, SDL_Renderer *renderer )
+SDL_Texture *loadTexture(std::string path, SDL_Renderer *renderer)
 {
-    //The final texture
-    SDL_Texture* newTexture = IMG_LoadTexture(renderer, path.c_str());
-    if( newTexture == NULL )
+    // The final texture
+    SDL_Texture *newTexture = IMG_LoadTexture(renderer, path.c_str());
+    if (newTexture == NULL)
     {
-        printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
     }
 
     return newTexture;
@@ -52,16 +52,16 @@ int main(int argc, char **argv)
     SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_Texture* manTexture = loadTexture("assets/man2.png", renderer);
-    SDL_Rect rectangle = { x: 0, y: 0, w: 64, h: 64};
+    SDL_Texture *manTexture = loadTexture("assets/man2.png", renderer);
+    SDL_Rect rectangle = {x : 0, y : 0, w : 64, h : 64};
 
     bool isRunning = true;
     SDL_Event event;
 
     std::unordered_set<SDL_Keycode> keys_pressed;
 
-    auto frame_length = std::chrono::microseconds{(int) (1.0/60.0*1000.0*1000.0)};
-    auto ticktocklength = std::chrono::seconds{(int) (1.0)};
+    auto frame_length = std::chrono::microseconds{(int)(1.0 / 60.0 * 1000.0 * 1000.0)};
+
     auto current_time = std::chrono::steady_clock::now();
 
     while (isRunning)
@@ -82,7 +82,8 @@ int main(int argc, char **argv)
                 else
                 {
                     bool inserted = keys_pressed.insert(event.key.keysym.sym).second;
-                    if (inserted) print_set(keys_pressed);
+                    if (inserted)
+                        print_set(keys_pressed);
                 }
                 break;
 
@@ -93,37 +94,37 @@ int main(int argc, char **argv)
             }
         }
 
-        if (keys_pressed.find(SDLK_UP) != keys_pressed.end())
+        while (std::chrono::steady_clock::now() > current_time + frame_length)
         {
-            rectangle.y -= 1;
-        }
-        if (keys_pressed.find(SDLK_DOWN) != keys_pressed.end())
-        {
-            rectangle.y += 1;
-        }
-        if (keys_pressed.find(SDLK_RIGHT) != keys_pressed.end())
-        {
-            rectangle.x += 1;
-        }
-        if (keys_pressed.find(SDLK_LEFT) != keys_pressed.end())
-        {
-            rectangle.x -= 1;
-        }
-        if (keys_pressed.find(SDLK_SPACE) != keys_pressed.end())
-        {
-            rectangle.x = 0;
-            rectangle.y = 0;
-        }
+            current_time += frame_length;
 
-        if (std::chrono::steady_clock::now() > current_time + ticktocklength) {
-            std::cout << "tick tock\n";
-            current_time += ticktocklength;
+            if (keys_pressed.find(SDLK_UP) != keys_pressed.end())
+            {
+                rectangle.y -= 5;
+            }
+            if (keys_pressed.find(SDLK_DOWN) != keys_pressed.end())
+            {
+                rectangle.y += 5;
+            }
+            if (keys_pressed.find(SDLK_RIGHT) != keys_pressed.end())
+            {
+                rectangle.x += 5;
+            }
+            if (keys_pressed.find(SDLK_LEFT) != keys_pressed.end())
+            {
+                rectangle.x -= 5;
+            }
+            if (keys_pressed.find(SDLK_SPACE) != keys_pressed.end())
+            {
+                rectangle.x = 0;
+                rectangle.y = 0;
+            }
         }
 
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 120, 140, 70, 255);
 
-        SDL_RenderCopy( renderer, manTexture, NULL, &rectangle );
+        SDL_RenderCopy(renderer, manTexture, NULL, &rectangle);
 
         SDL_RenderPresent(renderer);
     }
